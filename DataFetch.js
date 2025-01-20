9e00c07e33a480c0f57cc34257a4212d5208bac2
BugHunter-X <askzenai@gmail.com>
2025-01-20 08:46:08 +0100

    Add DataFetch.js file

---
+++ DataFetch.js
@@ -0,0 +1,37 @@
+```javascript
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, ActivityIndicator } from 'react-native';
+
+const DataFetch = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=20')
+      .then((response) => response.json())
+      .then((json) => setData(json.results))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <View>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ email }, index) => email}
+          renderItem={({ item }) => (
+            <View>
+              <Text>{item.name.first}</Text>
+            </View>
+          )}
+        />
+      )}
+    </View>
+  );
+};
+
+export default DataFetch;
+```
